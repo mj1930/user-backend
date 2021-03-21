adb1dd192f55a15f57705ce0da303ee5d871f846
Mihir Jain <mihir@chingari.io>
2021-03-21 15:57:39 +0530

    changes

--- routes/products.js
+++ routes/products.js
@@ -4,6 +4,9 @@ const productCtrl = require('../controllers/products');
 router.post('/get-all-products', productCtrl.listAllProduct);
 router.post('/get-products-by-category', productCtrl.findProductByCategory);
 router.post('/get-products-by-city', productCtrl.findProductByLocation);
+router.post('/get-products-by-price', productCtrl.filterProductsByPrice);
+router.post('/get-products-by-color', productCtrl.filterProductsByColors);
 router.get('/get-product-by-id', productCtrl.findProductById);
 router.get('/search-products', productCtrl.searchFromProducts);
+
 module.exports = router;
