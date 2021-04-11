add4c30aae92e76cde689e80f4d09577a302b7ef
Mihir Jain <mj3003@Mihirs-MacBook-Air.local>
2021-04-11 21:34:29 +0530

    added changes

--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,28 @@ const verification = require('./verification');
 const errorMiddleware = require('../middleware/error');
 
 module.exports = app => {
-    app.get('/', (req, res, next) => {
-        res.json({
-            code: 200,
-            title: 'Everything looks fine.'
-        });
-    });
+    // app.get('/', (req, res, next) => {
+    //     res.json({
+    //         code: 200,
+    //         title: 'Everything looks fine.'
+    //     });
+    // });
 
 
-    app.use((req, res, next) => {
-        if (
-            req.originalUrl.indexOf('/users') > -1 ||
-            req.originalUrl.indexOf('/products') > -1 ||
-            req.originalUrl.indexOf('/orders') > -1 ||
-            req.originalUrl.indexOf('/carts') > -1 ||
-            req.originalUrl.indexOf('/category') > -1 ||
-            req.originalUrl.indexOf('/verify') > -1
-        ) {
-            next()
-        } else {
-            return res.json({ code: 400, data: null, error: 'Beta Feature', message: 'This feature is currently unavailable.' });
-        }
-    });
+    // app.use((req, res, next) => {
+    //     if (
+    //         req.originalUrl.indexOf('/users') > -1 ||
+    //         req.originalUrl.indexOf('/products') > -1 ||
+    //         req.originalUrl.indexOf('/orders') > -1 ||
+    //         req.originalUrl.indexOf('/carts') > -1 ||
+    //         req.originalUrl.indexOf('/category') > -1 ||
+    //         req.originalUrl.indexOf('/verify') > -1
+    //     ) {
+    //         next()
+    //     } else {
+    //         return res.json({ code: 400, data: null, error: 'Beta Feature', message: 'This feature is currently unavailable.' });
+    //     }
+    // });
     app.use('/users', users);
     app.use('/products', product);
     app.use('/orders', orders);
